f1895cdd008ae08d74ca3173ab57af7d4900daac
Pannaga Rao Bhoja Ramamanohara <pannagaramamanohar@gmail.com>
2023-11-27 16:49:51 -0500

    Add test cases for login and bookmark

---
+++ Code/backend/__tests__/testLoginPage.js
@@ -0,0 +1,24 @@
+const request = require("supertest")("http://localhost:5000/api/v1/recipes");
+const expect = require("chai").expect;
+
+describe("Recipes API Tests", function () {
+
+  // Login Tests
+  describe("GET /login", function () {
+    it("should log in successfully with correct credentials", async function () {
+      const response = await request.get("/login")
+        .query({ userName: "Test", password: "admin" });
+
+      expect(response.status).to.eql(200);
+    });
+  });
+
+  describe("GET /signup", function () {
+    it("should not sign up for existing users", async function () {
+      const response = await request.get("/signup")
+        .query({ userName: "Test", password: "admin" });
+
+      expect(response.status).not.to.eql(200);
+    });
+  });
+});
