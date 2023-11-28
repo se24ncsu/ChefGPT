c2c3fdac9dd52a510d89cc36d352f10081e41cdc
Pannaga Rao Bhoja Ramamanohara <pannagaramamanohar@gmail.com>
2023-11-27 20:17:05 -0500

    Rename test file

--- Code/frontend/src/components/__tests__/Login.test.js
+++ Code/frontend/src/components/__tests__/Login.test.js
@@ -1,5 +1,5 @@
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
-import Login from "./../Login";
+import Login from "../Login";
 
 // Mock props
 const mockHandleLogin = jest.fn();
