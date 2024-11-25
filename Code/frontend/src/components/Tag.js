b43d0697ee53c0e42067a16afe45a58473ce8bde
akuldevali <akulg005@gmail.com>
2024-11-25 15:53:52 -0500

    Implemented Dark mode throught the app

--- Code/frontend/src/components/Tag.js
+++ Code/frontend/src/components/Tag.js
@@ -1,13 +1,21 @@
-import './css/misc.css';
-
 import React from 'react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 
 const Tag = (props) => {
+    const bgColor = useColorModeValue('#eee', '#333');
+    const selectedBgColor = useColorModeValue('#ccc', '#555');
 
     return (
-        <div style={{ padding: 5, background: props.selected ? '#ccc' : '#eee', fontSize: 12, margin: 2, borderRadius: 5 }}>
+        <Box
+            padding={1}
+            background={props.selected ? selectedBgColor : bgColor}
+            fontSize="12px"
+            margin={1}
+            borderRadius="5px"
+        >
             {props.children}
-        </div>
-    )
+        </Box>
+    );
 }
+
 export default Tag;
