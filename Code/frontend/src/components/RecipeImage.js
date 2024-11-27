fd9fdc7f0a8a367ae397e6d9c3d23411c72fa114
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-11-26 19:45:42 -0500

    RecipeImage linting

--- Code/frontend/src/components/RecipeImage.js
+++ Code/frontend/src/components/RecipeImage.js
@@ -1,51 +1,54 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './css/misc.css';
 import axios from 'axios';
 import { Oval } from 'react-loader-spinner';
 import { MdErrorOutline } from "react-icons/md";
-import { getCache, setCache } from '../globalcache';
+import { fetchRecipeImage, saveRecipeImage } from '../service/firestoreService';
 
 const RecipeImage = (props) => {
-
     const [url, setUrl] = useState('');
     const [loading, setLoading] = useState(true);
     const hoverEffects = props.hoverEffects === undefined ? true : props.hoverEffects;
 
     useEffect(() => {
-        var cached = getCache(`img:${props.name}`);
-        if (cached !== undefined) {
-            setUrl(cached);
-            setLoading(false);
-        }
-        else {
-            const data = {
-                name: props.name
-            };
-            setLoading(true);
-            axios.post(process.env.REACT_APP_GET_IMAGE_URL, data).then(response => {
-                setUrl(response.data);
+        const loadImage = async () => {
+            // Check Firestore for existing image URL
+            const cachedImage = await fetchRecipeImage(props.name);
+            if (cachedImage) {
+                setUrl(cachedImage);
                 setLoading(false);
-                setCache(`img:${props.name}`, response.data);
-            }).catch(() => {
-                setLoading(false);
-            });
-        }
+            } else {
+                const data = { name: props.name };
+                setLoading(true);
+                axios.post(process.env.REACT_APP_GET_IMAGE_URL, data).then(response => {
+                    setUrl(response.data);
+                    setLoading(false);
+                    // Save image URL to Firestore
+                    saveRecipeImage(props.name, response.data);
+                }).catch(() => {
+                    setLoading(false);
+                });
+            }
+        };
+
+        loadImage();
     }, [props.name]);
 
     if (loading) {
         return <div style={{ height: props.height, width: props.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Oval height={30} />
-        </div >;
+        </div>;
     }
 
     if (url === '') {
         return <div style={{ height: props.height, width: props.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <MdErrorOutline size={30} />
-        </div >;
+        </div>;
     }
 
     return (
         <img src={url} alt='' className={hoverEffects ? 'hover_scale' : ''} style={{ height: props.height, width: props.width, objectFit: 'cover', objectPosition: 'center' }} />
-    )
+    );
 }
+
 export default RecipeImage;
