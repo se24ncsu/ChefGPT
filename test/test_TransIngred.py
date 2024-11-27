453e4822d541b29e8d4656f4a6c27ffa904a7deb
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-11-27 01:12:27 -0500

    Delete test directory

--- test/test_TransIngred.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def test() :
-    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
-    assert len(result['recipes'][0]['TranslatedIngredients']) != 0
--- test/test_getingredient.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def test_get_results():
-    result = requests.get("http://localhost:5000/api/v1/recipes/callIngredients/")
-    assert result.status_code==200
